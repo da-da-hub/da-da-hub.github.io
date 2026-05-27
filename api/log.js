7beabcc9453b244015c6d49467a89b03cdb9b177
da-da-hub <71449405+da-da-hub@users.noreply.github.com>
2026-05-27 16:04:36 +0300

    Update log.js

--- api/log.js
+++ api/log.js
@@ -6,9 +6,9 @@ export default function handler(req, res) {
         ip: headers['x-forwarded-for'] || headers['x-real-ip'],
         user_agent: headers['user-agent'],
         referer: headers['referer'] || 'Direct',
-        accept_language: headers['accept-language']
+        accept_language: headers['accept-language'],
+        url: req.url
     });
-
-    res.writeHead(302, { Location: '/' });
-    res.end();
+    
+    res.status(200).json({ status: 'logged' });
 }
