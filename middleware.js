f5b78b343d74d71fef3cca89e779551fcbd5ddc7
da-da-hub <71449405+da-da-hub@users.noreply.github.com>
2026-05-27 17:09:12 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,20 +1,36 @@
 export default function middleware(request) {
   const allHeaders = Object.fromEntries(request.headers);
   
-
-  delete allHeaders['x-forwarded-for'];
-  delete allHeaders['x-real-ip'];
-
-
+  const excludeHeaders = [
+    'x-vercel-id',
+    'x-vercel-ip-as-number',
+    'x-vercel-ip-continent',
+    'x-vercel-ip-country',
+    'x-vercel-ip-latitude',
+    'x-vercel-ip-longitude',
+    'x-vercel-ip-timezone',
+    'x-vercel-ja4-digest',
+    'x-vercel-oidc-token',
+    'x-vercel-proxy-signature',
+    'x-vercel-deployment-url',
+    'x-forwarded-for',
+    'x-real-ip',
+    'x-forwarded-host',
+    'x-forwarded-proto'
+  ];
+  
+  for (const header of excludeHeaders) {
+    delete allHeaders[header];
+  }
+  
   console.log({
     timestamp: new Date().toISOString(),
     method: request.method,
     url: request.url,
     path: new URL(request.url).pathname,
     query: Object.fromEntries(new URL(request.url).searchParams),
-    headers: allHeaders,
+    headers: allHeaders
   });
 }
-
 
 export const config = { matcher: '/:path*' };
